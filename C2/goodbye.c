a63e5d210ebf17a0cc02c904352af2d02c797bce
simingw2209163 <simingw2209@163.com>
2025-03-01 22:55:18 +0000

    test weather mingw works

---
+++ C2/goodbye.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 7; i++) // Fix: Use `;` instead of `,`
+    
+        printf("Goodbye, Fang Datong.\n"); // Breakpoint here
+    return 0;
+}
