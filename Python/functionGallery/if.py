9f2e581997cc8ede58881c9ed6bd78987a095cbb
simingw2209163 <simingw2209@163.com>
2025-03-12 01:43:09 +0000

    f

---
+++ Python/functionGallery/if.py
@@ -0,0 +1,10 @@
+x = int(input("x: "))
+y = int(input("y: "))
+
+##boolen can be used with parentheses or without
+if (x<y):
+    print("x is less than y");
+elif x>y:
+    print("x is bigger than y")
+else:        
+    print("x equals to y")
---
+++ Python/functionGallery/withopen.py
@@ -0,0 +1,24 @@
+# # Open the original file with UTF-8 encoding
+# with open('WordsList.txt', 'r', encoding='utf-8') as file:
+#     words = file.read().splitlines()  # Read the lines and split by newline
+
+# # Add a comma after each word
+# words_with_commas = [word + ',' for word in words]
+
+# # Save the modified words into a new file
+# with open('WordsList.txt', 'w', encoding='utf-8') as file:
+#     file.write('\n'.join(words_with_commas))  # Join words with commas and write to the file
+
+
+# Open the original file with UTF-8 encoding
+with open('WordsList.txt', 'r', encoding='utf-8') as file:
+    words = file.read().splitlines()  # Read the lines and split by newline
+
+# Add double quotes and a comma after each word
+words_with_quotes_and_commas = [f'"{word}",' for word in words]
+
+# Save the modified words into a new file
+with open('WordsList.txt', 'w', encoding='utf-8') as file:
+    file.write('\n'.join(words_with_quotes_and_commas))  # Join words with quotes and commas, then write to the file
+    
+#######this script is also in HTMLCSSJS\AutoComplete\split.py
