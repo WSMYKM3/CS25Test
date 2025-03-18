e2bd9752982de4861e7caec0f63e6cf37c173077
simingw2209163 <simingw2209@163.com>
2025-03-18 14:57:53 +0000

    3

---
+++ Python/functionGallery/argvSys.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+if len(argv) == 2:
+    print(f"Hello, {argv[1]}")##this argvSys.py is the first, so if python argvSys.py David, David is the argv[1]
+else:
+    print("Hello, world")    
+    
