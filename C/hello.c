7130047e4abf520c6c0837d7ba6ce6c017a2e496
simingw2209163 <simingw2209@163.com>
2025-03-21 14:12:15 +0000

    delete

--- C/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    for (int i = 0; i < 5; i++) // Fix: Use `;` instead of `,`
-    
-        printf("Hello, World!\n"); // Breakpoint here
-    return 0;
-}
