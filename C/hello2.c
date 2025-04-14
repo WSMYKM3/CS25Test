db0a1ffbba3de2ba6c8934f813f9c3fcc93bc556
simingw2209163 <simingw2209@163.com>
2025-04-14 16:50:40 +0100

    ..

---
+++ C/hello2.c
@@ -0,0 +1,9 @@
+#include <cs50.h>//here I have not install the cs50 library
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string(What's your name?\n")
+    printf("hello ,%s\n", name);
+    return 0;
+}
